2748954a0843631639f32399da1160cb74cf346e
Rkarkoub03 <rfk5340@psu.edu>
2024-10-09 17:46:17 -0400

    adding GTAN files

--- Data_Prep.py
+++ Data_Prep.py
@@ -1,20 +1,27 @@
-import networkx as nx
-import matplotlib.pyplot as plt
 
-# Create an empty graph
-G = nx.Graph()
-
-# Connect nodes 1 to 10 in a circle (circular backbone)
-G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10), (10, 1)])
-
-# Connect all even-numbered nodes (2, 4, 6, 8, 10)
-even_nodes = [2, 4, 6, 8, 10]
-G.add_edges_from([(i, j) for i in even_nodes for j in even_nodes if i != j])
-
-# Connect all odd-numbered nodes (1, 3, 5, 7, 9)
-odd_nodes = [1, 3, 5, 7, 9]
-G.add_edges_from([(i, j) for i in odd_nodes for j in odd_nodes if i != j])
-
-# Draw the graph
-nx.draw(G, with_labels=True, node_color='lightblue', node_size=500, font_size=10)
-plt.show()
+#Prepare data for FFSD data set 
+def base_load_data(args: dict):
+    # load S-FFSD dataset for base models
+    data_path = "data/S-FFSD.csv"
+    feat_df = pd.read_csv(data_path)
+    train_size = 1 - args['test_size']
+    method = args['method']
+    # for ICONIP16 & AAAI20
+    if args['method'] == 'stan':
+        if os.path.exists("data/tel_3d.npy"):
+            return
+        features, labels = span_data_3d(feat_df)
+    else:
+        if os.path.exists("data/tel_2d.npy"):
+            return
+        features, labels = span_data_2d(feat_df)
+    num_trans = len(feat_df)
+    trf, tef, trl, tel = train_test_split(
+        features, labels, train_size=train_size, stratify=labels, shuffle=True)
+    trf_file, tef_file, trl_file, tel_file = args['trainfeature'], args[
+        'testfeature'], args['trainlabel'], args['testlabel']
+    np.save(trf_file, trf)
+    np.save(tef_file, tef)
+    np.save(trl_file, trl)
+    np.save(tel_file, tel)
+    return
